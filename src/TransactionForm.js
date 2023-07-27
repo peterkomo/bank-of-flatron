4b4bc0effad3f1f8dcfaa592ce80c7c8028abd88
peterkomo <mwangipeterkomo@gmail.com>
2023-07-27 11:31:55 +0300

    declared a function and passed in props

--- src/TransactionForm.js
+++ src/TransactionForm.js
@@ -1,1 +1,7 @@
 import React, { useState } from "react"; // Import React and useState hook.
+
+const TransactionForm = ({ onAddTransaction }) => {
+    const [date, setDate] = useState(""); // State for storing the date, initial value is an empty string.
+    const [description, setDescription] = useState(""); // State for storing the description, initial value is an empty string.
+    const [category, setCategory] = useState(""); // State for storing the category, initial value is an empty string.
+    const [amount, setAmount] = useState(""); // State for storing the amount, initial value is an empty string.
