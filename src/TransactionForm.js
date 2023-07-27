5542cd9cb2273229681c6beb3278df84695f8680
peterkomo <mwangipeterkomo@gmail.com>
2023-07-27 11:42:36 +0300

    converted the amount as a float value,useed the time strap as auique id

--- src/TransactionForm.js
+++ src/TransactionForm.js
@@ -5,3 +5,15 @@ const TransactionForm = ({ onAddTransaction }) => {
     const [description, setDescription] = useState(""); // State for storing the description, initial value is an empty string.
     const [category, setCategory] = useState(""); // State for storing the category, initial value is an empty string.
     const [amount, setAmount] = useState(""); // State for storing the amount, initial value is an empty string.
+// Function to handle form submission and add new transaction
+const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior.
+
+    const newTransaction = {
+        date,
+        description,
+        category,
+        amount: parseFloat(amount), // Convert the amount to a float value.
+        id: Date.now(), // Use the current timestamp as the unique ID.
+      };
+  
