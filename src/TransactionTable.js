7485480628c0472544338fe31720c0cf3904e687
peterkomo <mwangipeterkomo@gmail.com>
2023-07-27 12:00:08 +0300

    updated transaction table

--- src/TransactionTable.js
+++ src/TransactionTable.js
@@ -1,1 +1,35 @@
-export default TransactionForm; // Export the TransactionForm component.
+import React from "react";
+
+const TransactionTable = ({ transactions, onDeleteTransaction, onSort }) => {
+    return (
+      <table className="table ">
+        <thead>
+          <tr className="table-info">
+            <th onClick={() => onSort("date")}>Date</th>
+            <th onClick={() => onSort("description")}>Description</th>
+            <th onClick={() => onSort("category")}>Category</th>
+            <th onClick={() => onSort("amount")}>Amount</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className="table-success">
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>${transaction.amount}</td>
+              <td>
+                <button onClick={() => onDeleteTransaction(transaction.id)}>
+                  Delete
+                </button>
+                <button onClick={() => onSort(transaction.id)}>Sort</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  
+  export default TransactionTable; // Export the TransactionTable component.
